ccd368a53340e8c6eb9114c77cb459056fb8f3c3
Chondan <chondansusuwan@outlook.com>
2020-11-03 17:08:30 +0700

    uploaded build folder

---
+++ precache-manifest.4078a8cd21f5e822a5f1befbe21f8505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1502ec4c6cd13c435cac52365b303b",
+    "url": "/Redux-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "cd7b918a8be978150d41",
+    "url": "/Redux-Shopping-Cart/static/css/main.2edc7712.chunk.css"
+  },
+  {
+    "revision": "d928639e2e82fb77e851",
+    "url": "/Redux-Shopping-Cart/static/js/2.f917d08a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Redux-Shopping-Cart/static/js/2.f917d08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7b918a8be978150d41",
+    "url": "/Redux-Shopping-Cart/static/js/main.d2b0399d.chunk.js"
+  },
+  {
+    "revision": "2b7b17f4152a1982f517",
+    "url": "/Redux-Shopping-Cart/static/js/runtime-main.17aac5f6.js"
+  }
+]);
